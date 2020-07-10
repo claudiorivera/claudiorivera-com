d480c675992a50ea3fceca9dc3ef763e9f1e7afc
Claudio Rivera <me@claudiorivera.com>
2020-07-09 21:25:23 -0500

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Habit & Routine`,
+    description: `The official site of musician, drum tech, and developer, Claudio Rivera.`,
+    author: `Claudio Rivera`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Claudio Rivera`,
+        short_name: `Claudio Rivera`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cr-profile-pic.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,11 +3,11 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
+const BlogPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi</h1>
+    <SEO title="Blog" />
+    <h1>Blog</h1>
   </Layout>
 );
 
-export default IndexPage;
+export default BlogPage;
