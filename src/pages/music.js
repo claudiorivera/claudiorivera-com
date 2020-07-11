6eae1af7693f42ce2e145369690c989fdc2fea97
Claudio Rivera <me@claudiorivera.com>
2020-07-11 09:54:50 -0500

    Started music page

--- src/pages/music.js
+++ src/pages/music.js
@@ -6,7 +6,176 @@ import SEO from "../components/SEO";
 const MusicPage = () => (
   <Layout>
     <SEO title="Music" />
-    <h1>Music</h1>
+    <h1>Video</h1>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/videoseries?list=PLB953FCBE7D8E1AC1"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <h1>Audio</h1>
+    <figure>
+      <iframe
+        src="https://embed.music.apple.com/us/playlist/songs-ive-played-on/pl.u-MZrqIo3RAW?app=music"
+        height="450"
+      ></iframe>
+    </figure>
+    <h1>Selected Discography & Experience</h1>
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=http%3A%2F%2Fwww.savestheday.com&amp;t=MjljMmRiYzZhZmEzOWI5ZjgyODcyNTFjMGVmMDMxYjFiMmUyZTFjZCxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Saves The Day
+        </a>
+        &nbsp;
+      </strong>
+      (Equal Vision)
+    </p>
+
+    <ul>
+      <li>Drummer (2020 – Present)</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=https%3A%2F%2Fbit.ly%2Fhabitandroutine&amp;t=ZDQ5ZmNjNWVmNmIxZjczYmY1OTAwZTU1NzIyMTU0Zjk2YTA5NDA4YyxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          “Habit and Routine, Vol. 2”
+        </a>
+        &nbsp;
+      </strong>
+      (2020 – Self-released EP)
+    </p>
+
+    <ul>
+      <li>Performed drums, vocals, and guitars</li>
+      <li>Engineered and mixed</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=https%3A%2F%2Fbit.ly%2Fhabitandroutine&amp;t=ZDQ5ZmNjNWVmNmIxZjczYmY1OTAwZTU1NzIyMTU0Zjk2YTA5NDA4YyxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          “Habit and Routine”
+        </a>
+        &nbsp;
+      </strong>
+      (2019 – Self-released EP)
+    </p>
+
+    <ul>
+      <li>Performed drums, vocals, and guitars</li>
+      <li>Engineered and mixed</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=http%3A%2F%2Fwww.motioncitysoundtrack.com&amp;t=MGJmMTlmMjY1MmUyODkyYjJhNWYxNDkyYmI5ZGI2ZDQ4MjdmYjY5NixUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Motion City Soundtrack
+        </a>
+        &nbsp;
+      </strong>
+      (Epitaph)
+    </p>
+
+    <ul>
+      <li>Drummer (2013 – 2016)</li>
+      <li>Performed drums on LP “Panic Stations” (2015)</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=http%3A%2F%2Fwww.savestheday.com&amp;t=MjljMmRiYzZhZmEzOWI5ZjgyODcyNTFjMGVmMDMxYjFiMmUyZTFjZCxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Saves The Day
+        </a>
+        &nbsp;
+      </strong>
+      (Equal Vision)
+    </p>
+
+    <ul>
+      <li>Drummer (2010 – 2013)</li>
+      <li>Performed drums on LP “Saves The Day” (2013)</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJimmy_Robbins&amp;t=MjU2OGMyZTc3NDIzNWM3MTAwNDdiNWNmNjNlMzYxYjM4ZmIyNmU4YixUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Jimmy Robbins
+        </a>
+        &nbsp;
+      </strong>
+      (Universal/Motown)
+    </p>
+
+    <ul>
+      <li>Touring drummer (2009)</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=https%3A%2F%2Fitunes.apple.com%2Fus%2Fartist%2Fsomerset%2Fid102288119&amp;t=ZGE2ZDJjYmQxYjU4YmE4OGU4OGFkMTNjNGI0YTI4ZjJmYTQ1ZDBlZSxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Somerset
+        </a>
+        &nbsp;
+      </strong>
+      (Punknews)
+    </p>
+
+    <ul>
+      <li>Drummer (2002 – 2008)</li>
+      <li>Performed drums on LP “Pandora” (2005)</li>
+      <li>Performed drums on EP “Somerset” (2004)</li>
+      <li>Performed drums on EP “This Thought Process” (2003)</li>
+    </ul>
+
+    <p>
+      <strong>
+        <a
+          href="https://t.umblr.com/redirect?z=https%3A%2F%2Fitunes.apple.com%2Fus%2Fartist%2Fsing-it-loud%2Fid265101134&amp;t=YTZiNmExZjdkOTM0YjE2ZjhjY2IzYmY1NWFiOWFkMTMwMmUxNWMwZCxUbU4zdWpESg%3D%3D&amp;p=&amp;m=0"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Sing It Loud
+        </a>
+        &nbsp;
+      </strong>
+      (Epitaph)
+    </p>
+
+    <ul>
+      <li>Co-produced drums on LP “Come Around” (2008)</li>
+      <li>
+        Performed drums on song “Come Around” from LP “Come Around” (2008)
+      </li>
+    </ul>
   </Layout>
 );
 
