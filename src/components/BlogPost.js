a23e0924d399dc1303a0f5df280e2d0dd8c56e89
Claudio Rivera <me@claudiorivera.com>
2020-07-17 20:48:38 -0500

    Basic blog post component

---
+++ src/components/BlogPost.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Typography, Container } from "@material-ui/core";
+import Img from "gatsby-image";
+
+const BlogPost = ({ post }) => {
+  const { category, title, date, featuredImage } = post.frontmatter;
+
+  return (
+    <Container>
+      <Typography variant="h6">{category}</Typography>
+      <Typography variant="h5">{date}</Typography>
+      <Typography variant="h3">{title}</Typography>
+      <Img fluid={featuredImage.childImageSharp.fluid} />
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </Container>
+  );
+};
+
+export default BlogPost;
