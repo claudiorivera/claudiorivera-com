974b39523335174599fd1c8709efc76bb2e3dd68
Claudio Rivera <me@claudiorivera.com>
2020-09-27 11:21:28 -0500

    Consistent code style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,27 @@
-import { Box, Link, Typography } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import React from "react";
 
-const Footer = () => {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      p={8}
-      mt={7}
-      textAlign="center"
-      bgcolor="primary.main"
-      color="white"
-    >
-      <Typography variant="h5">
-        Made by me with Gatsby.&nbsp;
-        <a
-          href="https://github.com/claudiorivera/cr-com"
-          style={{ color: "white" }}
-        >
-          View Source
-        </a>
-      </Typography>
-    </Box>
-  );
-};
+const Footer = () => (
+  <Box
+    display="flex"
+    flexDirection="column"
+    justifyContent="center"
+    p={8}
+    mt={7}
+    textAlign="center"
+    bgcolor="primary.main"
+    color="white"
+  >
+    <Typography variant="h5">
+      Made by me with Gatsby.&nbsp;
+      <a
+        href="https://github.com/claudiorivera/cr-com"
+        style={{ color: "white" }}
+      >
+        View Source
+      </a>
+    </Typography>
+  </Box>
+);
 
 export default Footer;
