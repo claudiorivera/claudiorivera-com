ab9dd16365b7cfc817b441f3fac9320cf389c819
Claudio Rivera <me@claudiorivera.com>
2020-09-29 14:53:24 -0500

    Date string formatting

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -31,7 +31,7 @@ export const query = graphql`
       html
       frontmatter {
         title
-        date
+        date(formatString: "LL")
         category
         featuredImage {
           childImageSharp {
