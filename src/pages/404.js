e207ea20b54a5e2324a33cbb32d2f38c1c22e1ea
Claudio Rivera <me@claudiorivera.com>
2020-09-29 15:46:01 -0500

    Fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,37 @@
+import PropTypes from "prop-types";
 import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({ data }) => (
+  <Layout coverImage={data.file.childImageSharp.fluid} coverTitle="Uh Oh!">
     <SEO title="404: Not found" />
-    <h1>There's nothing here :(</h1>
+    <h1>There's nothing here. Sorry about that.</h1>
   </Layout>
 );
+
+export const query = graphql`
+  {
+    file(relativePath: { eq: "billow926-rTufXtvIFXc-unsplash.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+NotFoundPage.propTypes = {
+  data: PropTypes.shape({
+    file: PropTypes.shape({
+      childImageSharp: PropTypes.shape({
+        fluid: PropTypes.shape({
+          src: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default NotFoundPage;
