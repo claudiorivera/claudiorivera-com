d046ca9f133465c78a60e7f1f84cba68da42dc43
Claudio Rivera <me@claudiorivera.com>
2022-07-12 22:18:43 -0500

    🚧 Parse markdown to HTML

--- src/content/posts/heres-to-more-drums-this-year.md
+++ src/content/posts/heres-to-more-drums-this-year.md
@@ -5,6 +5,6 @@ category: "music"
 featuredImage: "/assets/blog/tumblr_p4a6vvrlPE1qz67oko1_1280.jpg"
 ---
 
-![Drums](/images/tumblr_p4a6vvrlPE1qz67oko2_1280.jpg)
+![Drums](/assets/blog/tumblr_p4a6vvrlPE1qz67oko2_1280.jpg)
 
 Here’s to more drums this year!
--- src/lib/postsApi.ts
+++ src/lib/postsApi.ts
@@ -2,11 +2,13 @@ import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
+import { markdownToHtml } from "./markdownToHtml";
+
 const postsDirectory = join(process.cwd(), "src/content/posts");
 
 export const getPostSlugs = () => fs.readdirSync(postsDirectory);
 
-export const getPostBySlug = (slug: string, fields: string[] = []) => {
+export const getPostBySlug = async (slug: string, fields: string[] = []) => {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -19,27 +21,34 @@ export const getPostBySlug = (slug: string, fields: string[] = []) => {
   const items: Items = {};
 
   // Ensure only the minimal needed data is exposed
-  fields.forEach((field) => {
+  for (const field of fields) {
     if (field === "slug") {
       items[field] = realSlug;
     }
     if (field === "content") {
-      items[field] = content;
+      items[field] = await markdownToHtml(content || "");
     }
 
     if (typeof data[field] !== "undefined") {
       items[field] = data[field];
     }
-  });
+  }
 
   return items;
 };
 
 export const getAllPosts = async (fields: string[] = []) => {
   const slugs = getPostSlugs();
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+
+  let posts = [];
+  for (const slug of slugs) {
+    const post = await getPostBySlug(slug, fields);
+    posts.push(post);
+  }
+
+  const sortedPosts = posts.sort((post1, post2) =>
+    post1.date > post2.date ? -1 : 1
+  );
+
+  return sortedPosts;
 };
