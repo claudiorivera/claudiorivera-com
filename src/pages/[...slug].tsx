0b2bb2abc83b1fed89423253cccd56c409239221
Claudio Rivera <me@claudiorivera.com>
2022-07-16 21:59:37 -0500

    🚧 Add prism and tweak styles

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
+import dayjs from "dayjs";
 import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
 import { PostType } from "types/post";
@@ -29,6 +30,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const post = await getPostBySlug(params?.slug as string, [
     "slug",
     "title",
+    "category",
     "date",
     "featuredImage",
     "content",
@@ -46,12 +48,17 @@ type Props = {
 };
 
 const BlogPost = ({ post }: Props) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   if (!post) return null;
 
   return (
     <Layout coverImage={post.featuredImage} coverTitle={post.title}>
-      <h2>{post.date}</h2>
+      {isMobile && <Typography variant="h2">{post.title}</Typography>}
       <Typography variant="overline">{post.category}</Typography>
+      <Typography variant="h3">
+        {dayjs(post.date).format("MMMM d, YYYY")}
+      </Typography>
       <Typography
         variant="body1"
         component="div"
