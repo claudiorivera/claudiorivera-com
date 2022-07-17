4b4c34081dfbf5af15770663a1782f9eed151bd8
Claudio Rivera <me@claudiorivera.com>
2022-07-17 10:21:57 -0500

    ✨ Finish migrating to Next.js

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
-import Obfuscate from "react-obfuscate";
+
+import Link from "./Link";
 
 const Footer = () => (
   <Box
@@ -10,34 +11,25 @@ const Footer = () => (
     mt={7}
     textAlign="center"
     bgcolor="primary.main"
-    color="white"
   >
-    <Typography variant="h5">
-      <a
+    <Typography sx={{ color: "white" }} variant="h5">
+      <Link
+        sx={{ color: "white" }}
         href="https://github.com/claudiorivera/claudiorivera-com"
-        style={{ color: "white" }}
       >
-        <span>Made by me. Here&apos;s the source</span>
-      </a>
+        Site made by me
+      </Link>
       {". "}
-      <span>
-        Email&nbsp;
-        <Obfuscate
-          style={{ color: "white" }}
-          email="me@claudiorivera.com"
-          headers={{
-            subject: "Hi Claudio",
-          }}
-        />
-        .
-      </span>{" "}
-      Connect with me on{" "}
-      <a
+      <Link sx={{ color: "white" }} href="mailto:me@claudiorivera.com">
+        Email me
+      </Link>{" "}
+      or connect on{" "}
+      <Link
+        sx={{ color: "white" }}
         href="https://www.linkedin.com/in/atclaudiorivera/"
-        style={{ color: "white" }}
       >
         LinkedIn
-      </a>
+      </Link>
       .
     </Typography>
   </Box>
