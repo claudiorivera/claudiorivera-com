bc27a79774bd47d73377954ef33ada5fdd219a90
Claudio Rivera <me@claudiorivera.com>
2022-07-17 20:34:07 -0500

    🚧 Fix static rendering of pages beyond /blog

--- src/pages/blog/[page].tsx
+++ src/pages/blog/[page].tsx
@@ -14,14 +14,17 @@ import { getAllPosts } from "@/lib/postsApi";
 export const getStaticPaths: GetStaticPaths = async () => {
   const { pageContext } = await getAllPosts({
     fields: ["slug"],
+    postsPerPage: 3,
   });
 
   const { numPages } = pageContext;
 
+  const pageNumbers = [...Array(numPages)].map((_, i) => i + 1);
+
   return {
-    paths: Array.from({ length: numPages }, (_, i) => ({
+    paths: pageNumbers.map((pageNumber) => ({
       params: {
-        page: `page-${i + 1}`,
+        page: `page-${pageNumber}`,
       },
     })),
     fallback: true, // false or 'blocking'
@@ -29,12 +32,16 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const page = (params?.page as string).split("-")[1] || "1";
+  const page = (params?.page as string).split("-")[1];
   const { posts, pageContext } = await getAllPosts({
     fields: ["slug", "title", "date", "category", "featuredImage", "content"],
     page: parseInt(page),
     postsPerPage: 3,
   });
+
+  if (!posts) {
+    return { props: {} };
+  }
 
   return {
     props: {
@@ -48,54 +55,58 @@ type Props = {
   posts: PostType[];
   pageContext: PageContext;
 };
-const BlogPosts = ({ posts, pageContext }: Props) => (
-  <Layout
-    coverImage="/images/patrick-fore-0gkw_9fy0eQ-unsplash.jpg"
-    coverTitle="Blog"
-  >
-    <Seo title="Blog" />
-    {posts.map((post) => {
-      const { slug, title, date, category, featuredImage, content } = post;
-      const postYear = date.split("-")[0];
-      const postMonth = date.split("-")[1];
-      const postDay = date.split("-")[2];
+const BlogPosts = ({ posts, pageContext }: Props) => {
+  if (!posts) return null;
 
-      return (
-        <Fragment key={slug}>
-          <Container>
-            <Box style={{ textAlign: "center" }}>
-              <Typography variant="overline">{category}</Typography>
-              <Link href={`/${postYear}/${postMonth}/${postDay}/${slug}`}>
-                <Typography variant="h2">{title}</Typography>
-              </Link>
-              <Typography variant="subtitle2" my={1.5}>
-                {dayjs(date).format("MMMM D, YYYY")}
-              </Typography>
-            </Box>
-            <Box sx={{ width: "100%", height: "auto", position: "relative" }}>
-              <Image
-                style={{
-                  width: "100%",
-                  height: "auto",
-                }}
-                src={featuredImage}
-                width={600}
-                height={400}
-                alt=""
+  return (
+    <Layout
+      coverImage="/images/patrick-fore-0gkw_9fy0eQ-unsplash.jpg"
+      coverTitle="Blog"
+    >
+      <Seo title="Blog" />
+      {posts.map((post) => {
+        const { slug, title, date, category, featuredImage, content } = post;
+        const postYear = date.split("-")[0];
+        const postMonth = date.split("-")[1];
+        const postDay = date.split("-")[2];
+
+        return (
+          <Fragment key={slug}>
+            <Container>
+              <Box style={{ textAlign: "center" }}>
+                <Typography variant="overline">{category}</Typography>
+                <Link href={`/${postYear}/${postMonth}/${postDay}/${slug}`}>
+                  <Typography variant="h2">{title}</Typography>
+                </Link>
+                <Typography variant="subtitle2" my={1.5}>
+                  {dayjs(date).format("MMMM D, YYYY")}
+                </Typography>
+              </Box>
+              <Box sx={{ width: "100%", height: "auto", position: "relative" }}>
+                <Image
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  src={featuredImage}
+                  width={600}
+                  height={400}
+                  alt=""
+                />
+              </Box>
+              <Typography
+                variant="body1"
+                component="div"
+                dangerouslySetInnerHTML={{ __html: content }}
               />
-            </Box>
-            <Typography
-              variant="body1"
-              component="div"
-              dangerouslySetInnerHTML={{ __html: content }}
-            />
-          </Container>
-          <hr style={{ marginBlock: "4rem" }} />
-        </Fragment>
-      );
-    })}
-    <BlogPagination pageContext={pageContext} />
-  </Layout>
-);
+            </Container>
+            <hr style={{ marginBlock: "4rem" }} />
+          </Fragment>
+        );
+      })}
+      <BlogPagination pageContext={pageContext} />
+    </Layout>
+  );
+};
 
 export default BlogPosts;
