579db79846e001c129d4d0e85a372e636baf599d
Claudio Rivera <me@claudiorivera.com>
2025-02-09 19:08:30 -0600

    ⬆️ Bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.claudiorivera.com",
-	integrations: [
-		mdx(),
-		sitemap(),
-		react(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
+	integrations: [mdx(), sitemap(), react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	output: "static",
 	adapter: vercel({
 		webAnalytics: {
